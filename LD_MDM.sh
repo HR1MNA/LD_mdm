1d8d063c23f8d738d937e11b3e49455f6d62a92a
HR1MNA <129339956+HR1MNA@users.noreply.github.com>
2024-09-27 11:23:29 -0600

    Update LD_MDM.sh

--- LD_MDM.sh
+++ LD_MDM.sh
@@ -1,184 +1,309 @@
 #!/bin/bash
 
-# Definir códigos de color
-RED='\033[1;31m'
-GRN='\033[1;32m'
-BLU='\033[1;34m'
-YEL='\033[1;33m'
-PUR='\033[1;35m'
-CYAN='\033[1;36m'
-NC='\033[0m'
+credit() {
+    for i in {1..97}
+    do
+       echo ""
+    done
+    echo "============================================="
+    echo "             MDM Tools by Kaitiz             "
+    echo "============================================="
+}
 
-# Mostrar encabezado
-echo -e "${CYAN}MDM & Notification Management Script by Assaf Dori & HR1MNA${NC}"
-echo ""
+show_menu() {
+    credit
+    echo ""
+    echo "1. First Setup (Recovery)"
+    echo "2. Disable MDM Notification"
+    echo "3. Check MDM Enrollment (Terminal)"
+    echo "4. Quit"
+    echo ""
+}
 
-# Función para mostrar el menú de nuevo
-mostrar_menu() {
-    PS3='Por favor, elige una opción: '
-    options=("Bypass MDM desde Recovery" "Deshabilitar Notificaciones (SIP)" "Deshabilitar Notificaciones (Recovery)" "Verificar inscripción en MDM" "Configurar Launch Daemon para Bloqueo Permanente" "Reiniciar y Salir")
-    select opt in "${options[@]}"; do
-        case $opt in
-            "Bypass MDM desde Recovery")
-                echo -e "${YEL}Bypass MDM desde Recovery${NC}"
-                system_volume="/Volumes/macOS Base System"
+show_first_setup_menu() {
+    for i in {1..97}
+    do
+       echo ""
+    done
+    echo "============================================="
+    echo "           First Setup (Recovery)            "
+    echo "============================================="
+    echo ""
+    echo "1. Set Root Password (Recovery)"
+    echo "2. Apple Setup Done (Recovery)"
+    echo "3. Disable Root User (Terminal)"
+    echo "4. Back to Main Menu"
+    echo ""
+}
 
-                # Verificar si la partición de datos está montada
-                if [ -d "$system_volume - Data" ]; then
-                    diskutil rename "$system_volume - Data" "Data"
+show_disable_notification_menu() {
+    for i in {1..97}
+    do
+       echo ""
+    done
+    echo "============================================="
+    echo "         MDM Enrollment Notification         "
+    echo "============================================="
+    echo ""
+    echo "1. Disable DEP (Recovery)"
+    echo "2. Disable DEP (Terminal - Requires SIP to be disabled)"
+    echo "3. Check /etc/hosts file"
+    echo "4. Back to Main Menu"
+    echo ""
+}
+
+handle_choice() {
+    case $1 in
+        1)
+            handle_first_setup
+            ;;
+        2)
+            handle_disable_notification
+            ;;
+        3)
+            for i in {1..97}
+            do
+               echo ""
+            done
+            echo "============================================="
+            echo "            Check MDM Enrollment             "
+            echo "============================================="
+            echo ""
+            sudo profiles status -type enrollment
+            echo ""
+            sudo profiles show -type enrollment
+            echo ""
+            echo "- Note: Error is success -"
+            echo ""
+            read -p "Please press Enter to continue..."
+            ;;
+        4)
+            echo "Quit. Goodbye!"
+            exit 0
+            ;;
+        *)
+            echo "Invalid choice. Please select again."
+            ;;
+    esac
+}
+
+ask_block_hosts() {
+    for i in {1..97}
+    do
+       echo ""
+    done
+    read -p "Do you want to block hosts? (Y/N): " block_choice
+    case $block_choice in
+        [Yy]* )
+            echo ""
+            sudo sed -i '' '/# MDM Servers/d' /Volumes/Macintosh\ HD/etc/hosts
+            sudo sed -i '' '/# End/d' /Volumes/Macintosh\ HD/etc/hosts
+            sudo sed -i '' '/deviceenrollment.apple.com/d' /Volumes/Macintosh\ HD/etc/hosts
+            sudo sed -i '' '/mdmenrollment.apple.com/d' /Volumes/Macintosh\ HD/etc/hosts
+            sudo sed -i '' '/iprofiles.apple.com/d' /Volumes/Macintosh\ HD/etc/hosts
+            sudo sed -i '' '/acmdm.apple.com/d' /Volumes/Macintosh\ HD/etc/hosts
+            sudo sed -i '' '/axm-adm-mdm.apple.com/d' /Volumes/Macintosh\ HD/etc/hosts
+
+            echo "# MDM Servers" | sudo tee -a /Volumes/Macintosh\ HD/etc/hosts
+            echo "0.0.0.0 deviceenrollment.apple.com" | sudo tee -a /Volumes/Macintosh\ HD/etc/hosts
+            echo "0.0.0.0 mdmenrollment.apple.com" | sudo tee -a /Volumes/Macintosh\ HD/etc/hosts
+            echo "0.0.0.0 iprofiles.apple.com" | sudo tee -a /Volumes/Macintosh\ HD/etc/hosts
+            echo "0.0.0.0 acmdm.apple.com" | sudo tee -a /Volumes/Macintosh\ HD/etc/hosts
+            echo "0.0.0.0 axm-adm-mdm.apple.com" | sudo tee -a /Volumes/Macintosh\ HD/etc/hosts
+            echo "# End" | sudo tee -a /Volumes/Macintosh\ HD/etc/hosts
+
+            echo ""
+            echo "Blocked all MDM servers!"
+            echo ""
+            ;;
+        [Nn]* )
+            echo ""
+            echo "Skipping host blocking."
+            ;;
+        * )
+            echo "Invalid choice. Please enter Y or N."
+            ask_block_hosts
+            ;;
+    esac
+}
+
+ask_block_hosts_recovery() {
+    for i in {1..97}
+    do
+       echo ""
+    done
+    read -p "Do you want to block hosts? (Y/N): " block_recovery_choice
+    case $block_recovery_choice in
+        [Yy]* )
+            echo ""
+            sed -i '' '/# MDM Servers/d' /Volumes/Macintosh\ HD/etc/hosts
+            sed -i '' '/# End/d' /Volumes/Macintosh\ HD/etc/hosts
+            sed -i '' '/deviceenrollment.apple.com/d' /Volumes/Macintosh\ HD/etc/hosts
+            sed -i '' '/mdmenrollment.apple.com/d' /Volumes/Macintosh\ HD/etc/hosts
+            sed -i '' '/iprofiles.apple.com/d' /Volumes/Macintosh\ HD/etc/hosts
+            sed -i '' '/acmdm.apple.com/d' /Volumes/Macintosh\ HD/etc/hosts
+            sed -i '' '/axm-adm-mdm.apple.com/d' /Volumes/Macintosh\ HD/etc/hosts
+
+            echo "# MDM Servers" >> /Volumes/Macintosh\ HD/etc/hosts
+            echo "0.0.0.0 deviceenrollment.apple.com" >> /Volumes/Macintosh\ HD/etc/hosts
+            echo "0.0.0.0 mdmenrollment.apple.com" >> /Volumes/Macintosh\ HD/etc/hosts
+            echo "0.0.0.0 iprofiles.apple.com" >> /Volumes/Macintosh\ HD/etc/hosts
+            echo "0.0.0.0 acmdm.apple.com" >> /Volumes/Macintosh\ HD/etc/hosts
+            echo "0.0.0.0 axm-adm-mdm.apple.com" >> /Volumes/Macintosh\ HD/etc/hosts
+            echo "# End" >> /Volumes/Macintosh\ HD/etc/hosts
+
+            echo ""
+            echo "Blocked all MDM servers!"
+            echo ""
+            ;;
+        [Nn]* )
+            echo ""
+            echo "Skipping host blocking."
+            ;;
+        * )
+            echo "Invalid choice. Please enter Y or N."
+            ask_block_hosts_recovery
+            ;;
+    esac
+}
+
+handle_first_setup() {
+    while true; do
+        show_first_setup_menu
+        read -p "Please choose one option (1-4): " sub_choice
+        for i in {1..97}
+        do
+           echo ""
+        done
+        case $sub_choice in
+            1)
+                for i in {1..97}
+                do
+                   echo ""
+                done
+                if [ -d "/Volumes/Macintosh HD - Data" ]; then
+                    diskutil rename "Macintosh HD - Data" "Data"
                 fi
-
-                # Verificar si la carpeta de usuario ya existe
-                if [ ! -d "$system_volume/Users/Apple" ]; then
-                    mkdir "$system_volume/Users/Apple"
-                else
-                    echo -e "${YEL}El directorio $system_volume/Users/Apple ya existe.${NC}"
-                fi
-
-                # Bloquear dominios MDM en /etc/hosts
-                echo "Bloqueando dominios MDM en /etc/hosts..."
-                if ! grep -q "deviceenrollment.apple.com" "$system_volume/etc/hosts"; then
-                    echo "0.0.0.0 deviceenrollment.apple.com" >> "$system_volume/etc/hosts"
-                fi
-                if ! grep -q "mdmenrollment.apple.com" "$system_volume/etc/hosts"; then
-                    echo "0.0.0.0 mdmenrollment.apple.com" >> "$system_volume/etc/hosts"
-                fi
-                if ! grep -q "iprofiles.apple.com" "$system_volume/etc/hosts"; then
-                    echo "0.0.0.0 iprofiles.apple.com" >> "$system_volume/etc/hosts"
-                fi
-                if ! grep -q "acmdm.apple.com" "$system_volume/etc/hosts"; then
-                    echo "0.0.0.0 acmdm.apple.com" >> "$system_volume/etc/hosts"
-                fi
-                if ! grep -q "axm-adm-mdm.apple.com" "$system_volume/etc/hosts"; then
-                    echo "0.0.0.0 axm-adm-mdm.apple.com" >> "$system_volume/etc/hosts"
-                fi
-
-                echo -e "${GRN}Dominios MDM bloqueados exitosamente${NC}"
-
-                # Eliminar perfiles de configuración
-                echo "Eliminando perfiles de configuración..."
-                touch "$system_volume/private/var/db/.AppleSetupDone"
-                rm -rf "$system_volume/var/db/ConfigurationProfiles/Settings/.cloudConfigHasActivationRecord"
-                rm -rf "$system_volume/var/db/ConfigurationProfiles/Settings/.cloudConfigRecordFound"
-                touch "$system_volume/var/db/ConfigurationProfiles/Settings/.cloudConfigProfileInstalled"
-                touch "$system_volume/var/db/ConfigurationProfiles/Settings/.cloudConfigRecordNotFound"
-                echo -e "${GRN}Perfiles de configuración eliminados exitosamente.${NC}"
-
-                echo -e "${GRN}El MDM ha sido evitado exitosamente!${NC}"
-                echo -e "${NC}Cierra la terminal y reinicia tu Mac.${NC}"
-                mostrar_menu # Volver a mostrar el menú
+                echo ""
+                dscl -f /Volumes/Data/private/var/db/dslocal/nodes/Default localhost -passwd /Local/Default/Users/root
+                echo ""
+                ask_block_hosts_recovery
+                echo ""
+                echo "1. Please restart your Mac and select a language..."
+                echo "2. Press 'Command + Option + Control + T' at the same time to open Terminal."
+                echo "3. In Terminal, go to 'System Settings -> Users & Groups'."
+                echo "4. Select 'Add User' and input the username as 'root' and the password as the password you set above."
+                echo "5. Create an admin user account, then reboot your Mac to recovery, run the script again, and select 'Apple Setup Done'."
+                echo ""
+                read -p "Do you want to reboot now? (Y/N): " reboot_choice
+                case $reboot_choice in
+                    [Yy]* )
+                        reboot
+                        ;;
+                    *)
+                        echo "Returning to main menu..."
+                        ;;
+                esac
                 ;;
-
-            "Deshabilitar Notificaciones (SIP)")
-                echo -e "${RED}Introduce tu contraseña para proceder${NC}"
-                rm /var/db/ConfigurationProfiles/Settings/.cloudConfigHasActivationRecord
-                rm /var/db/ConfigurationProfiles/Settings/.cloudConfigRecordFound
-                touch /var/db/ConfigurationProfiles/Settings/.cloudConfigProfileInstalled
-                touch /var/db/ConfigurationProfiles/Settings/.cloudConfigRecordNotFound
-                echo -e "${GRN}Notificaciones deshabilitadas exitosamente (SIP)${NC}"
-                mostrar_menu # Volver a mostrar el menú
-                ;;
-
-            "Deshabilitar Notificaciones (Recovery)")
-                rm -rf "$system_volume/var/db/ConfigurationProfiles/Settings/.cloudConfigHasActivationRecord"
-                rm -rf "$system_volume/var/db/ConfigurationProfiles/Settings/.cloudConfigRecordFound"
-                touch "$system_volume/var/db/ConfigurationProfiles/Settings/.cloudConfigProfileInstalled"
-                touch "$system_volume/var/db/ConfigurationProfiles/Settings/.cloudConfigRecordNotFound"
-                echo -e "${GRN}Notificaciones deshabilitadas exitosamente desde Recovery${NC}"
-                mostrar_menu # Volver a mostrar el menú
-                ;;
-
-            "Verificar inscripción en MDM")
-                echo -e "${GRN}Verificación de inscripción en MDM${NC}"
-                profiles show -type enrollment
-                mostrar_menu # Volver a mostrar el menú
-                ;;
-
-            "Configurar Launch Daemon para Bloqueo Permanente")
-                if [ ! -d "/usr/local/bin" ]; then
-                    echo "Creando el directorio /usr/local/bin"
-                    mkdir -p /usr/local/bin
+            2)
+                for i in {1..97}
+                do
+                   echo ""
+                done
+                if [ -d "/Volumes/Macintosh HD - Data" ]; then
+                    diskutil rename "Macintosh HD - Data" "Data"
                 fi
-
-                bloque_script="/usr/local/bin/bloquear_mdm.sh"
-                echo -e "${GRN}Creando el script de bloqueo en $bloque_script${NC}"
-                tee "$bloque_script" > /dev/null << EOF
-#!/bin/bash
-
-system_volume="/Volumes/macOS Base System"
-echo "Bloqueando dominios MDM en /etc/hosts..."
-if ! grep -q "deviceenrollment.apple.com" "\$system_volume/etc/hosts"; then
-    echo "0.0.0.0 deviceenrollment.apple.com" >> "\$system_volume/etc/hosts"
-fi
-if ! grep -q "mdmenrollment.apple.com" "\$system_volume/etc/hosts"; then
-    echo "0.0.0.0 mdmenrollment.apple.com" >> "\$system_volume/etc/hosts"
-fi
-if ! grep -q "iprofiles.apple.com" "\$system_volume/etc/hosts"; then
-    echo "0.0.0.0 iprofiles.apple.com" >> "\$system_volume/etc/hosts"
-fi
-if ! grep -q "acmdm.apple.com" "\$system_volume/etc/hosts"; then
-    echo "0.0.0.0 acmdm.apple.com" >> "\$system_volume/etc/hosts"
-fi
-if ! grep -q "axm-adm-mdm.apple.com" "\$system_volume/etc/hosts"; then
-    echo "0.0.0.0 axm-adm-mdm.apple.com" >> "\$system_volume/etc/hosts"
-fi
-EOF
-
-                chmod +x "$bloque_script"
-
-                plist_file="/Library/LaunchDaemons/com.usuario.bloquear_mdm.plist"
-                echo -e "${GRN}Creando el archivo plist para el Launch Daemon en $plist_file${NC}"
-                tee "$plist_file" > /dev/null << EOF
-<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-<plist version="1.0">
-  <dict>
-    <key>Label</key>
-    <string>com.usuario.bloquear_mdm</string>
-
-    <key>ProgramArguments</key>
-    <array>
-      <string>/bin/bash</string>
-      <string>$bloque_script</string>
-    </array>
-
-    <key>RunAtLoad</key>
-    <true/>
-
-    <key>StartInterval</key>
-    <integer>3600</integer> <!-- Ejecuta el script cada hora -->
-
-    <key>StandardOutPath</key>
-    <string>/var/log/bloquear_mdm.log</string>
-    <key>StandardErrorPath</key>
-    <string>/var/log/bloquear_mdm.err</string>
-  </dict>
-</plist>
-EOF
-
-                chown root:wheel "$plist_file"
-                chmod 644 "$plist_file"
-
-                echo -e "${GRN}Cargando el Launch Daemon${NC}"
-                launchctl load "$plist_file"
-
-                echo -e "${GRN}Launch Daemon configurado y cargado exitosamente.${NC}"
-                mostrar_menu # Volver a mostrar el menú
+                touch /Volumes/Data/private/var/db/.AppleSetupDone
+                echo ""
+                rm -rf /Volumes/Macintosh\ HD/var/db/ConfigurationProfiles/Settings/.cloudConfigHasActivationRecord
+                rm -rf /Volumes/Macintosh\ HD/var/db/ConfigurationProfiles/Settings/.cloudConfigRecordFound
+                touch /Volumes/Macintosh\ HD/var/db/ConfigurationProfiles/Settings/.cloudConfigProfileInstalled
+                touch /Volumes/Macintosh\ HD/var/db/ConfigurationProfiles/Settings/.cloudConfigRecordNotFound
+                ask_block_hosts_recovery
+                echo ""
+                read -p "Do you want to reboot now? (Y/N): " reboot_choice
+                case $reboot_choice in
+                    [Yy]* )
+                        reboot
+                        ;;
+                    *)
+                        echo "Returning to main menu..."
+                        ;;
+                esac
                 ;;
-
-            "Reiniciar y Salir")
-                echo "Reiniciando..."
-                reboot
-                break
+            3)
+                for i in {1..97}
+                do
+                   echo ""
+                done
+                dsenableroot -d
+                echo ""
+                read -p "Please press Enter to continue..."
                 ;;
-
-            *) 
-                echo "Opción no válida: $REPLY"
-                mostrar_menu
+            4)
+                echo "Returning to the main menu."
+                return
+                ;;
+            *)
+                echo "Invalid choice. Please select again."
                 ;;
         esac
     done
 }
 
-# Mostrar el menú por primera vez
-mostrar_menu
+handle_disable_notification() {
+    while true; do
+        show_disable_notification_menu
+        read -p "Please choose one option (1-4): " noti_choice
+        for i in {1..97}
+        do
+           echo ""
+        done
+        case $noti_choice in
+            1)
+                for i in {1..97}
+                do
+                   echo ""
+                done
+                rm -rf /Volumes/Macintosh\ HD/var/db/ConfigurationProfiles/Settings/.cloudConfigHasActivationRecord
+                rm -rf /Volumes/Macintosh\ HD/var/db/ConfigurationProfiles/Settings/.cloudConfigRecordFound
+                touch /Volumes/Macintosh\ HD/var/db/ConfigurationProfiles/Settings/.cloudConfigProfileInstalled
+                touch /Volumes/Macintosh\ HD/var/db/ConfigurationProfiles/Settings/.cloudConfigRecordNotFound
+                ask_block_hosts_recovery
+                echo ""
+                read -p "Please press Enter to continue..."
+                ;;
+            2)
+                for i in {1..97}
+                do
+                   echo ""
+                done
+                sudo rm /var/db/ConfigurationProfiles/Settings/.cloudConfigHasActivationRecord
+                sudo rm /var/db/ConfigurationProfiles/Settings/.cloudConfigRecordFound
+                sudo touch /var/db/ConfigurationProfiles/Settings/.cloudConfigProfileInstalled
+                sudo touch /var/db/ConfigurationProfiles/Settings/.cloudConfigRecordNotFound
+                ask_block_hosts
+                echo ""
+                read -p "Please press Enter to continue..."
+                ;;
+            3)
+                cat /Volumes/Macintosh\ HD/etc/hosts
+                echo ""
+                read -p "Please press Enter to continue..."
+                ;;
+            4)
+                echo "Returning to the main menu."
+                return
+                ;;
+            *)
+                echo "Invalid choice. Please select again."
+                ;;
+        esac
+    done
+}
+
+while true; do
+    show_menu
+    read -p "Please choose one option (1-4): " choice
+    handle_choice $choice
+done
